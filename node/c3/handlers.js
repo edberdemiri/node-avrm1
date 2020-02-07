5d3ea80954c89c10368d4404ea68e3385cdf0693
edberdemiri <43780186+edberdemiri@users.noreply.github.com>
2020-02-07 17:00:36 +0100

    z

---
+++ node/c3/handlers.js
@@ -0,0 +1,53 @@
+const index = (req,res)=>{
+    res.send('ok');
+};
+
+const ime =(req,res)=>{
+    res.send('pero');
+};
+const greetings = (req,res)=>{
+    res.send(req.params.ime);
+};
+
+const calc1 = (req,res)=>{
+    let a = 0;
+      if(req.params.meth == 'add'){
+          a = Number(req.params.num1) + Number(req.params.num2);        
+      }
+      if(req.params.meth == 'div'){
+          a = Number(req.params.num1) / Number(req.params.num2);        
+      }
+      if(req.params.meth == 'mul'){
+          a = Number(req.params.num1) * Number(req.params.num2);    
+      }
+      if(req.params.meth == 'sub'){
+          a = Number(req.params.num1) - Number(req.params.num2);        
+      }
+      res.send(''+a);
+  };
+
+  const calc2 = (req,res)=>{
+    let a = 0;
+      if(req.body.meth == 'add'){
+          a = Number(req.body.num1) + Number(req.body.num2);        
+      }
+      if(req.body.meth == 'div'){
+          a = Number(req.body.num1) / Number(req.body.num2);        
+      }
+      if(req.body.meth == 'mul'){
+          a = Number(req.body.num1) * Number(req.body.num2);    
+      }
+      if(req.body.meth == 'sub'){
+          a = Number(req.body.num1) - Number(req.body.num2);        
+      }
+      res.send(''+a);
+  };
+
+
+module.exports={
+    index,
+    ime,
+    greetings,
+    calc1,
+    calc2
+};
--- node/c3/index.js
+++ node/c3/index.js
@@ -1,60 +1,20 @@
 const express = require ('express');
 const bodyParses= require('body-parser');
-
-
-
+const handlers = require ('handlers');
 var app = express();
-
 app.use(bodyParses.urlencoded({extended:false})); // reading body from post request 
-app.get ('/',(req,res)=>{
-    res.send('ok');
-});
 
-app.get ('/ime',(req,res)=>{
-    res.send('pero');
-});
+app.get ('/',handlers.index);
+app.get ('/ime',handlers.ime);
+app.get ('/page/:ime',handlers.greetings);
 
-app.get ('/page/:ime',(req,res)=>{
-    res.send(req.params.ime);
-});
-
-app.get ('/calc/:meth/:num1/:num2',(req,res)=>{
-  let a = 0;
-    if(req.params.meth == 'add'){
-        a = Number(req.params.num1) + Number(req.params.num2);        
-    }
-    if(req.params.meth == 'div'){
-        a = Number(req.params.num1) / Number(req.params.num2);        
-    }
-    if(req.params.meth == 'mul'){
-        a = Number(req.params.num1) * Number(req.params.num2);    
-    }
-    if(req.params.meth == 'sub'){
-        a = Number(req.params.num1) - Number(req.params.num2);        
-    }
-    res.send(''+a);
-});
+app.get ('/calc/:meth/:num1/:num2',handlers.calc1);
 
 app.post('/values', (req,res)=>{
  res.send(`response:'+ ${req.body.age} + ${req.body.ime}`);
 })
 
-app.post ('/calc/',(req,res)=>{
-    let a = 0;
-      if(req.body.meth == 'add'){
-          a = Number(req.body.num1) + Number(req.body.num2);        
-      }
-      if(req.body.meth == 'div'){
-          a = Number(req.body.num1) / Number(req.body.num2);        
-      }
-      if(req.body.meth == 'mul'){
-          a = Number(req.body.num1) * Number(req.body.num2);    
-      }
-      if(req.body.meth == 'sub'){
-          a = Number(req.body.num1) - Number(req.body.num2);        
-      }
-      res.send(''+a);
-  });
+app.post ('/calc/',handlers.calc2);
 
 
 app.listen(8080);
